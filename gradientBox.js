2488d1d778b99151defdd9e2ecd06417d1659daa
Jay Kim <kimjaehu@gmail.com>
2021-04-06 22:10:10 -0400

    add files

--- gradientBox.js
+++ gradientBox.js
@@ -1,16 +1,10 @@
 export class GradientBox {
-  constructor(y, img) {
+  constructor(y) {
     this.y = y;
     this.vy = Math.random() * 16 - 8;
-    // this.minHeight = 800;
-    // this.maxHeight = 1400;
-    this.img = img;
-    
-    this.minHeight = 400;
+    this.minHeight = 800;
     this.maxHeight = 1400;
-
     this.height = this.randomHeight();
-
   }
 
   resize(stageWidth, stageHeight) {
@@ -18,24 +12,27 @@ export class GradientBox {
     this.stageHeight = stageHeight;
   }
 
-  draw(ctx, color, translateX, translateY, scale) {
+  draw(ctx, color, stageWidth, stageHeight, translateX, translateY, scale) {
+    this.y += this.vy;
 
-    // this.y += this.vy;
+    if (this.y < 0 - stageWidth / scale) {
+      this.vy *= -1;
+      this.y += 10;
+    } else if (this.y > stageHeight + this.height / scale) {
+      this.vy *= -1;
+      this.y -= 10;
+    }
 
-    // if (this.y < this.stageWidth * 0.5) {
-    //   this.vy *= -1;
-    //   this.y += 10;
-    // } else if (this.y > this.stageHeight + this.height) {
-    //   this.vy *= -1;
-    //   this.y -= 10;
-    // }
-    
+    ctx.save();
+    ctx.translate((stageWidth - translateX) / scale, -translateY);
+    ctx.rotate((Math.PI / 180) * 120);
+
     ctx.fillStyle = `rgb(${color.r},${color.g},${color.b})`;
 
     const grd = ctx.createLinearGradient(
-      this.stageWidth * 0.5,
+      stageWidth / 2,
       this.y,
-      this.stageWidth * 0.5,
+      stageWidth / 2,
       this.y + this.height
     );
 
@@ -44,38 +41,14 @@ export class GradientBox {
     grd.addColorStop(1, `rgba(${color.r},${color.g},${color.b}, 0)`);
 
     ctx.fillStyle = grd;
-    // ctx.fillRect(
-    //   0,
-    //   this.y,
-    //   Math.sqrt(this.stageWidth * this.stageWidth + this.stageHeight * this.stageHeight) / scale,
-    //   this.height
-    // );
-
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.drawImage(this.img, this.stageWidth * 0.5, this.stageHeight * 0.75)
-
-    
-
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    
-    // ctx.translate((this.stageWidth - translateX) / scale, -translateY);
-    // ctx.translate(this.stageWidth * 0.5, this.stageHeight * 0.5);
-    ctx.translate(0, this.stageHeight);
-    ctx.rotate((Math.PI / 180) * -50);
-
-    ctx.globalCompositeOperation = 'source-in';
-
     ctx.fillRect(
-      0, this.y, 
-      // Math.sqrt(this.stageWidth * this.stageWidth + this.stageHeight * this.stageHeight), 
-      this.stageWidth,
+      0,
+      this.y,
+      Math.sqrt(stageWidth * stageWidth + stageHeight * stageHeight) / scale,
       this.height
-    )
-   
-    
-    
+    );
+    ctx.restore();
   }
-
 
   randomHeight() {
     return Math.random() * (this.maxHeight - this.minHeight) + this.minHeight;
